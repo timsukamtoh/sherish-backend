05e0afa064b397bc970fb9b801fe204fbf4b59a6
Timothy Sukamtoh <timtoh168@gmail.com>
2024-05-17 12:45:32 -0700

    included product attributes

--- database/sherish-schema.sql
+++ database/sherish-schema.sql
@@ -1,8 +1,9 @@
 CREATE TABLE users (
-    username VARCHAR (25) PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR (25) NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
+    first_name TEXT,
+    last_name TEXT,
     email TEXT NOT NULL UNIQUE
         CHECK (position('@' IN email) > 1),
     image_url TEXT,
@@ -17,8 +18,25 @@ CREATE TABLE categories (
 );
 
 CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    username REFERENCES users ON DELETE CASCADE,
-    product_url TEXT
-)
+    product_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    review TEXT,
+    rating DECIMAL(2, 1) NOT NULL CHECK (rating >= 0.0 AND rating <= 5.0),
+    product_url TEXT,
+    user_id VARCHAR(255) REFERENCES users(user_id),
+    category_id INTEGER REFERENCES categories(category_id),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
+CREATE TABLE attributes (
+    attribute_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL CHECK (name = lower(name))
+    -- e.g., "Sustainability", "Color", "Material"
+);
+
+CREATE TABLE product_attributes (
+    product_id INTEGER REFERENCES products(product_id),
+    attribute_id INTEGER REFERENCES attributes(attribute_id),
+    value TEXT NOT NULL, -- This could be "Eco-Friendly", "Red", etc.
+    PRIMARY KEY (product_id, attribute_id)
+);
